eef56004e302a23a1573d2b7059da921e4fc4e9b
Gabrielbemo <gabrielbemoura@gmail.com>
2025-03-17 18:11:08 -0300

    adding and config keycloak

--- services/notification/src/main/java/com/gabriel/ecommerce/email/EmailService.java
+++ services/notification/src/main/java/com/gabriel/ecommerce/email/EmailService.java
@@ -4,7 +4,6 @@ import com.gabriel.ecommerce.kafka.order.Product;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -23,7 +22,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_RELATED;
 
 @Service
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class EmailService {
 
